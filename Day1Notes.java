2398883a98629edc82286621e7601041b7226818
sandeep <sandeepkumarbaliarsingh863@gmail.com>
2026-05-25 22:05:37 +0530

    day 1 - Learned types of programming languages

---
+++ Day1Notes.java
@@ -0,0 +1,13 @@
+public class Day1Notes {
+    public static void main(String[] args) {
+        
+        // 3 types of programming languages
+        System.out.println("1. Procedural - step by step instructions");
+        System.out.println("2. Object Oriented - based on objects and classes");
+        System.out.println("3. Functional - based on mathematical functions");
+        
+        // static vs dynamic
+        System.out.println("Static - data type fixed at compile time eg. Java");
+        System.out.println("Dynamic - data type decided at runtime eg. Python");
+    }
+}
